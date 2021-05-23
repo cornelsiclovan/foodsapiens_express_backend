e289be305970eb4e075402d8c3c9f1de05c3ece7
iMac <imac@iMacs-iMac.local>
2021-05-23 22:15:53 +0300

    model and post from wp db

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -1,9 +1,10 @@
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const HttpError = require('../models/http-errors');
-
+const Fawn = require('fawn');
 const Order = require('../models/order');
 
+Fawn.init(mongoose);
 
 const getOrders = async (req, res, next) => {
     let orders;
@@ -36,16 +37,50 @@ const createOrder = async (req, res, next) => {
         return next(new HttpError('Invalid inputs passed, please check your data', 422));
     }
 
-    console.log(req.body);
+    console.log("post");
+    //console.log(req.body);
 
-    // const {
-    //     name,
-    //     address1,
-    //     address2,
-    //     city
-    // } = req.body;
+     const {
+         wp_id,
+         name,
+         address1,
+         address2,
+         city,
+         items,
+     } = req.body;
 
-    // console.log(name);
+    const createdOrder = new Order({
+        wp_id,
+        name,
+        address1,
+        address2,
+        city,
+        items
+    });     
+
+    let existingOrder;
+
+    try {
+        existingOrder = await Order.findOne({ wp_id: wp_id })
+    } catch (err) {
+        return next(new HttpError('Please try again later. Service unavailable', 500));
+    }
+
+    if(existingOrder) {
+        return next(new HttpError('This order already exists'))
+    }
+
+    try {
+        new Fawn.Task()
+            .save('orders', createdOrder)
+            .run();
+    } catch(error) {
+        error = new HttpError(error, 500);
+        return next(error);
+    }
+    
+    res.status(200).json({order: createdOrder});
+    console.log(createdOrder);
 }
 
 exports.getOrders = getOrders;
--- models/order.js
+++ models/order.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
+    wp_id : {
+        type: Number
+    },
     name: {
         type: String
     },
